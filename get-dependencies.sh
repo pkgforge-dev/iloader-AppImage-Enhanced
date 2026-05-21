f38435b14535d207e1fbe812211c8d3bac3fd335
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-05-21 05:58:48 -0400

    add `webkit2gtk-4.1`

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm webkit2gtk-4.1
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
